ca488a87443c92fd0d652d365f2921a760aa1b5d
MunirahAbdilah <145731805+MunirahAbdilah@users.noreply.github.com>
2024-03-15 13:31:19 +0800

    Cari dimensi gambar : width & height

--- quiz1.py
+++ quiz1.py
@@ -7,10 +7,20 @@ def grayscale(image):
 
   return gray
 
+def get_dimensions(image):
+
 image = cv2.imread("avatar.png")
+
+if image is None:
+    print("Error: Could not read image 'avatar.png'")
+    exit(1)
 
 gray_image = grayscale(image)
 
+width, height = get_dimensions(image)
+
+print("Grayscale image dimensions: width =", width, ", height =", height)
+
 cv2.imshow("Grayscale Image", gray_image)
-cv2.waitKey(0)
+cv2.waitKey(0) 
 cv2.destroyAllWindows()
